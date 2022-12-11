6496da91491ec8a7c436c4c111c790d20cbd59af
sapientum8 <sapientum08@gmail.com>
2022-12-10 23:00:42 -0500

    initial commit

---
+++ test101.py
@@ -0,0 +1,16 @@
+class MyClass:
+    def __len__(self):
+        return 0
+
+
+def print_hi(name):
+    a = {}
+    b = 2
+    myobj = MyClass()
+    print(f'Hi, {name}')
+    print(len(myobj))
+    print(1 if 2 else b)
+
+
+if __name__ == '__main__':
+    print_hi('20KPTS75')
